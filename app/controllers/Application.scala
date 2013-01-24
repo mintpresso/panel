683cf2b098a596c7a0e1a6db5603b2d84836bf66
Jinhyuk Lee <eces@mstock.org>
2013-01-24 19:03:02 +0900

    Add basic pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,27 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index())
-  }  
+    Ok(views.html.overview())
+  }
+
+  def features = Action {
+    Ok(views.html.features())
+  }
+
+  def docs = Action {
+    Ok(views.html.docs())
+  }
+
+  def plans = Action {
+    Ok(views.html.plans())
+  }
+
+  def login = Action {
+    Ok(views.html.login())
+  }
+
+  def company = Action {
+    Ok(views.html.company())
+  }
+
 }
