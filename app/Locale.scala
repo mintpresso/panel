b026f418145bbcdb0c8b1d476cf3ec01db5ce7f2
Jinhyuk Lee <eces@mstock.org>
2013-02-02 19:22:07 +0900

    Fix big

--- app/Locale.scala
+++ app/Locale.scala
@@ -5,6 +5,10 @@ import play.api.i18n.{Lang, Messages}
 object Locale {
   def getDateString(timestamp: Long)(implicit lang: Lang): String = {
     val f = new java.text.SimpleDateFormat( Messages("date.fullstring")(lang) )
-    return f.format( new java.sql.Timestamp(timestamp) )
+    if(timestamp == -1){
+      f.format( new java.sql.Timestamp(System.currentTimeMillis) )
+    }else{
+      f.format( new java.sql.Timestamp(timestamp) )
+    }
   }
 }
